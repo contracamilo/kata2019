fcf58cdded214254bb56ca1d8c4de7dfa86721cd
Camilo Rivera <camilo.riveraq@gmail.com>
2019-01-07 22:04:09 -0500

    new kata

--- januarykatas.js
+++ januarykatas.js
@@ -63,3 +63,17 @@ const stray = numbers => numbers.reduce((a, b) => a ^ b);
 // ^ Por cada posición de bit en la cual los bits correspondientes de ambos operandos son iguales devuelve cero y si son diferentes devuelve uno.
 
 
+
+//Issue 4 --------------------------------------------------------------------------
+
+//Welcome. In this kata, you are asked to square every digit of a number.
+//For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+//Note: The function accepts an integer and returns an integer
+
+
+function squareDigits(num){
+    var result = String(num).split('').map((x)=>x*x).join('')
+    return parseInt(result)
+}
+
+
