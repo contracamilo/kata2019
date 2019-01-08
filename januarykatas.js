22a8e3a55bec8a0d6ce5861cf979a18e97a72c5c
Camilo Rivera <camilo.riveraq@gmail.com>
2019-01-08 05:59:26 -0500

    new issue

--- januarykatas.js
+++ januarykatas.js
@@ -77,3 +77,20 @@ function squareDigits(num){
 }
 
 
+
+
+//Issue 5 --------------------------------------------------------------------------
+
+
+//You get an array of numbers, return the sum of all of the positives ones.
+//Example [1,-4,7,12] => 1 + 7 + 12 = 20
+//Note: if there is nothing to sum, the sum is default to 0.
+
+
+function positiveSum(arr) {
+    return arr.reduce((value, accum) => accum > 0 ? value + accum : value, 0)
+}
+//Other way to write this ES6:
+const positiveSum = (arr) => arr.reduce((value, accum) => accum > 0 ? value + accum : value, 0)
+
+
