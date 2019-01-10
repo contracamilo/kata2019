142d56b530a5b61acb10f9362ba3f640bc8b8a03
Camilo Rivera <camilo.riveraq@gmail.com>
2019-01-10 14:10:36 -0500

    issue 10

--- januarykatas.js
+++ januarykatas.js
@@ -143,3 +143,38 @@ function showSequence (count) {
 	  }
   }
 
+//Issue 10 --------------------------------------------------------------------------
+
+
+// Complete the method which accepts an array of integers, and returns one of the following:
+
+// "yes, ascending" - if the numbers in the array are sorted in an ascending order
+// "yes, descending" - if the numbers in the array are sorted in a descending order
+// "no" - otherwise
+
+
+//mine
+function isSortedAndHow(array) {
+    let arr = array
+    let arrStr = arr.join()
+    let desc = arr.sort((a, b)=> b-a).join()
+    let asce = arr.sort((a, b)=> a-b).join()
+    if(arrStr === desc){
+        return 'yes, descending'
+    }else if(arrStr === asce){
+        return 'yes, ascending'
+    }else{
+        return 'no' 
+    }
+
+}
+
+//optimized way
+
+const isSortedAndHow = array => {
+    let ascending = array.filter((e, i, a) => e > a[i+1]).length == 0
+    let descending = array.filter((e, i, a) => e < a[i+1]).length == 0
+    
+    return ascending ? 'yes, ascending' : descending ? 'yes, descending' : 'no'
+}
+
