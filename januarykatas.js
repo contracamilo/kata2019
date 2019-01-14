7c9e57c423d89da2c905a057df96fccc9ae8fff9
Camilo Rivera <camilo.riveraq@gmail.com>
2019-01-13 20:18:36 -0500

    mensual goal complete!

--- januarykatas.js
+++ januarykatas.js
@@ -306,3 +306,17 @@ const predictAge = (age1,age2,age3,age4,age5,age6,age7,age8) => {
     return  Math.round(Math.sqrt(arr.reduce((ac, acc) =>  ac + acc,0)) / 2)
 }
 
+
+//Issue 16 --------------------------------------------------------------------------
+
+/*
+In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+Example
+filter_list([1,2,'a','b']) == [1,2]
+filter_list([1,'a','b',0,15]) == [1,0,15]
+filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+*/
+
+const filter_list = (l) => l.filter((x) => (typeof x === 'number') ? Number(x >= 0):'') 
+
