164548791a93bbc4ef5151c6a3ac09a6a22e29d7
Camilo Rivera <camilo.riveraq@gmail.com>
2019-01-16 14:06:01 -0500

    commenting

--- januaryExtended.js
+++ januaryExtended.js
@@ -1,14 +1,11 @@
-Extra Issue 1 --------------------
-
+//Extra Issue 1 --------------------
 //remove duplicates from an array or string provided as a param.
 
 const uniqueInOrder = (iterable)=> return (typeof iterable == 'string')  ?  [...new Set( iterable.split('') )] : [...new Set(iterable)];
 
 
 
-Extra Issue 2 --------------------
-
-/*
+/*Extra Issue 2 --------------------
 Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
 uniqueInOrder('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
 uniqueInOrder('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
